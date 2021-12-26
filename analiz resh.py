a9c79f3ea6892fa2c7653843cc5688bda1095a43
shiriaeva <Elena.Shiriaeva@urfu.me>
2021-12-26 22:21:33 +0500

    dz

--- analiz resh.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import pandas as pd
-import matplotlib as plt
-
-works = pd.read_csv('works.csv', encoding='utf-8')
-
-print(f"общее количество записей в датасете = { works.shape[0] }")
-
